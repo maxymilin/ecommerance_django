7b42faca4c41e6a6f0f3aa6f399d202b7688ac2b
Maxym Ilin <maxym.ilin@uvik.net>
2022-11-04 13:59:44 +0200

    Create Category and Product models. Migrate this models.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    """A model that defines a category of the product in the database.
+    """
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """A model with main data of different products.
+    Has a foreign key from the 'Category' table to show a type of product
+    and from the 'User' table to show who has created the product.
+    """
+    category = models.ForeignKey(
+        Category,
+        related_name="product",
+        on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        User,
+        related_name="product_creator",
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default="admin")
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/")
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name_plural = "products"
+        ordering = ("-created", )
+
+    def __str__(self):
+        return self.title
