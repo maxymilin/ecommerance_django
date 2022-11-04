f94badbd8a0dd2f522d3a6dce33812c02adf907e
Maxym Ilin <maxym.ilin@uvik.net>
2022-11-04 18:59:49 +0200

    Create view, that shows all categories from store-categories table. This view is avaliable for each store page.

--- store/views.py
+++ store/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 from .models import Category, Product
 
 
+def categories(request):
+    """Get all Categories from the "store_categories" table.
+    Can access it from each app page.
+    """
+    return {
+        "categories": Category.objects.all()
+    }
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, "store/home.html", {"products": products})
