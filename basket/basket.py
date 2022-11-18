5697ec4558749b658560930333fc7b6d40ec8365
Maxym Ilin <maxym.ilin@uvik.net>
2022-11-18 16:37:39 +0200

    Create product adding to basket functionality. Addeing basket class what depends of session, addeing script what added product when you push button 'ADD' on product page. Create wiew for it and seperated url for adding products.

--- basket/basket.py
+++ basket/basket.py
@@ -12,7 +12,17 @@ class Basket():
         empty dictionary and set to session.
         """
         self.session = request.session
-        basket = self.session.get("session_key")
-        if "session_key" not in request.session:
-            basket = self.session["session_key"] = {'number': 123123123}
+        basket = self.session.get("s_key")
+        if "s_key" not in request.session:
+            basket = self.session["s_key"] = {}
         self.basket = basket
+
+    def add(self, product):
+        """Adding and updating the users baket session data.
+        """
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {"price": str(product.price)}
+
+        self.session.modified = True
--- basket/urls.py
+++ basket/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = "busket"
+app_name = "basket"
 
 urlpatterns = [
     path("", views.basket_summary, name="basket_summary"),
+    path("add/", views.basket_add, name="basket_add")
 ]
