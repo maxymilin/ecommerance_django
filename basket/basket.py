f31933485ae899e102606689b1e3d46332abc932
Maxym Ilin <maxym.ilin@uvik.net>
2022-11-18 11:21:02 +0200

    Create the Basket class to store byind product in user session.

---
+++ basket/basket.py
@@ -0,0 +1,19 @@
+
+
+class Basket():
+    """
+    A base Basket class, providing some default behaviors that
+    can be inherited or overrided, as necassary.
+    """
+
+    def __init__(self, request):
+        """Initialise the user session and get items in basket
+        by using session key. If no basket in session, create new
+        empty dictionary and set to session.
+        """
+        self.session = request.session
+        basket = self.session.get("session_key")
+        if "session_key" not in request.session:
+            basket = self.session["session_key"] = {}
+        self.basket = basket
+
